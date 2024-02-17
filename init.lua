e4d74a999f44e4270f59222bd2ea1e462c14faf5
Huska, Matthew <HuskaM@rki.de>
2024-02-17 22:34:38 +0100

    Minimal init.lua to get nextflow_config tree-sitter installed

---
+++ init.lua
@@ -0,0 +1,36 @@
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+-- Example using a list of specs with the default options
+vim.g.mapleader = " " -- Make sure to set `mapleader` before lazy so your mappings are correct
+
+require("lazy").setup({
+  {
+    "nvim-treesitter/nvim-treesitter",
+    event = { "BufReadPost", "BufNewFile" },
+    tag = "v0.9.2",
+    cmd = { "TSInstall", "TSBufEnable", "TSBufDisable", "TSModuleInfo" },
+    build = ":TSUpdate",
+  },
+})
+
+local parser_config = require "nvim-treesitter.parsers".get_parser_configs()
+
+parser_config.nextflow_config = {
+  install_info = {
+    url = "https://github.com/matthuska/tree-sitter-nextflow-config",
+    files = { "src/parser.c" },
+    branch = "main",
+  }
+}
+
